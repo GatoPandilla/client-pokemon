99711d943156f25f8e37429a272090824f28092f
jharoldm <jharol.eps@gmail.com>
2023-09-04 18:41:47 -0500

    pokemons

--- src/pages/Welcome/Welcome.tsx
+++ src/pages/Welcome/Welcome.tsx
@@ -1,9 +1,33 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
+const PokemonList = () => {
+  const [pokemons, setPokemons] = useState<string[]>([]);
 
-const Welcome = () => {
+  const fetchAllPokemons = async () => {
+    try {
+      const response = await axios.get('http://localhost:3000/pokemon');
+      const allPokemons: string[] = response.data.map((pokemon: { name: string }) => pokemon.name);
+      setPokemons(allPokemons);
+    } catch (error) {
+      console.error('Error al obtener todos los Pokémon:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchAllPokemons();
+  }, []);
+
   return (
-    <div>Welcome</div>
-  )
-}
+    <div>
+      <h1>Lista de Pokémon</h1>
+      <ul>
+        {pokemons.map((pokemon) => (
+          <li key={pokemon}>{pokemon}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-export default Welcome
+export default PokemonList;
